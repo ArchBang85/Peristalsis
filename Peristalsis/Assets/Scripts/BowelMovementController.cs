cb240c18935fab041db246dc3f101ecd2e16a5fb
Petri Autio <archbang85@gmail.com>
2015-11-28 17:13:51 +0000

    Boilerplate scene with basic piston functionality

---
+++ Peristalsis/Assets/Scripts/BowelMovementController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BowelMovementController : MonoBehaviour {
+
+    public List<GameObject> pistons = new List<GameObject>();
+    public float[] pistonSpeeds;
+    public float maxPistonReach = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+
+        pistonSpeeds = new float[3];
+        pistonSpeeds[0] = 1;
+        pistonSpeeds[1] = 0.5f;
+        pistonSpeeds[2] = 0.1f;
+
+	    foreach(GameObject piston in GameObject.FindGameObjectsWithTag("Piston"))
+        {
+            pistons.Add(piston);
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Move pistons
+        foreach(GameObject Piston in pistons)
+        {
+            PistonScript p = Piston.GetComponent<PistonScript>();
+            float t = p.trajectory;
+            // Reverse dir
+            if (p.forward && t > maxPistonReach)
+            {
+                
+                p.forward = false;
+
+            } 
+
+            if (!p.forward && t < -maxPistonReach)
+            {
+                p.forward = true;
+            }
+            float moveAmount = pistonSpeeds[p.group - 1] * Time.deltaTime;
+
+            if(p.forward)
+            {
+                // move forward
+
+                p.trajectory += moveAmount;
+                p.transform.Translate(Vector3.up * moveAmount);
+
+            } else
+            {
+                // move backwards by speed
+                p.trajectory -= moveAmount;
+                p.transform.Translate(Vector3.up * -moveAmount);
+
+            }
+       
+        }
+
+	
+	}
+}
---
+++ Peristalsis/Assets/Scripts/PistonScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PistonScript : MonoBehaviour {
+
+    public int group = 1;
+    public float trajectory = 0f;
+    public bool forward = true;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
