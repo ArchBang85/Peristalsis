58265c3c595f8b9415569c301d0d8b2e04d02fc2
Petri Autio <archbang85@gmail.com>
2015-11-29 13:29:50 +0000

    Added control sliders.

--- Peristalsis/Assets/Scripts/BowelMovementController.cs
+++ Peristalsis/Assets/Scripts/BowelMovementController.cs
@@ -8,14 +8,24 @@ public class BowelMovementController : MonoBehaviour {
     public List<GameObject> pistons = new List<GameObject>();
     public float[] pistonSpeeds;
     public float maxPistonReach = 0.4f;
+    public GameObject[] sliderPegs = new GameObject[3];
+    private float[] pegStartPos;
+    private float maxSpeed = 0.75f;
 
 	// Use this for initialization
 	void Start () {
 
         pistonSpeeds = new float[3];
-        pistonSpeeds[0] = 0.7f;
-        pistonSpeeds[1] = 0.6f;
-        pistonSpeeds[2] = 0.52f;
+        pistonSpeeds[0] = 0.0f;
+        pistonSpeeds[1] = 0.0f;
+        pistonSpeeds[2] = 0.0f;
+
+        pegStartPos = new float[3];
+        pegStartPos[0] = sliderPegs[0].transform.position.y;
+        pegStartPos[1] = sliderPegs[1].transform.position.y;
+        pegStartPos[2] = sliderPegs[2].transform.position.y;
+
+
 
 	    foreach(GameObject piston in GameObject.FindGameObjectsWithTag("Piston"))
         {
@@ -27,6 +37,7 @@ public class BowelMovementController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 
         // Move pistons
         foreach(GameObject Piston in pistons)
@@ -65,5 +76,48 @@ public class BowelMovementController : MonoBehaviour {
         }
 
 	
+
+        // Keyboard controls
+        if(Input.GetKey(KeyCode.Q) && pistonSpeeds[0] < maxSpeed)
+        {
+            sliderPegs[0].transform.position = new Vector2(sliderPegs[0].transform.position.x, pegStartPos[0] + pistonSpeeds[0]);
+            pistonSpeeds[0] += Time.deltaTime;
+            Mathf.Clamp(pistonSpeeds[0], -maxSpeed, maxSpeed);
+
+        }
+        if (Input.GetKey(KeyCode.A) && pistonSpeeds[0] > -maxSpeed)
+        {
+            sliderPegs[0].transform.position = new Vector2(sliderPegs[0].transform.position.x, pegStartPos[0] + pistonSpeeds[0]);
+            pistonSpeeds[0] -= Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W) && pistonSpeeds[1] < maxSpeed)
+        {
+            sliderPegs[1].transform.position = new Vector2(sliderPegs[1].transform.position.x, pegStartPos[1] + pistonSpeeds[1]);
+            pistonSpeeds[1] += Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) && pistonSpeeds[1] > -maxSpeed)
+        {
+            sliderPegs[1].transform.position = new Vector2(sliderPegs[1].transform.position.x, pegStartPos[1] + pistonSpeeds[1]);
+            pistonSpeeds[1] -= Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.E) && pistonSpeeds[2] < maxSpeed)
+        {
+            sliderPegs[2].transform.position = new Vector2(sliderPegs[2].transform.position.x, pegStartPos[2] + pistonSpeeds[2]);
+            pistonSpeeds[2] += Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D) && pistonSpeeds[2] > -maxSpeed)
+        {
+            sliderPegs[2].transform.position = new Vector2(sliderPegs[2].transform.position.x, pegStartPos[2] + pistonSpeeds[2]);
+            pistonSpeeds[2] -= Time.deltaTime;
+
+        }
+
+
+        Mathf.Clamp(pistonSpeeds[0], -maxSpeed, maxSpeed);
+        Mathf.Clamp(pistonSpeeds[1], -maxSpeed, maxSpeed);
+        Mathf.Clamp(pistonSpeeds[2], -maxSpeed, maxSpeed);
 	}
 }
